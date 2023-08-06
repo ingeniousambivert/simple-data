8bfdf919bf2587535bb76fe6cefdc248fecc490e
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2023-08-06 18:04:51 -0400

    add cli args feature for selecting steps

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 print("\nInitiating...\n")
 import os
+import sys
 from dotenv import load_dotenv
 from modules.extract import extract_data
 from modules.clean import clean_data
@@ -16,42 +17,73 @@ if __name__ == "__main__":
         "customFields": os.environ["CUSTOM_FIELDS"],
         "tags": os.environ["TAGS_URL"],
         "pipelines": os.environ["PIPELINES_URL"],
-        # "opportunities": os.environ["OPPORTUNITIES_URL"], # TODO - Need pipeline ids
     }
     os.system("cls" if os.name == "nt" else "clear")
     print("\n___  HUMBER COLLEGE  ___")
     print("______   Group 7  ______\n")
     print("Data Analytics Wizard :\n")
 
-    user_endpoints = input(
-        "Enter entities (contacts,campaigns,tags... or 'all' for all entities): "
-    ).split(",")
-    user_output_format = input("Enter output format (csv or xlsx): ")
-    user_limit = input(
-        "Enter the maximum number of data points to fetch (or leave empty for all): "
-    )
+    if len(sys.argv) > 1 and sys.argv[1] == "extract":
+        user_endpoints = input(
+            "Enter entities (contacts,campaigns,tags... or 'all' for all entities): "
+        ).split(",")
+        user_output_format = input("Enter output format (csv or xlsx): ")
+        user_limit = input(
+            "Enter the maximum number of data points to fetch (or leave empty for all): "
+        )
 
-    if "all" in user_endpoints:
-        user_endpoints = endpoints.keys()
+        if "all" in user_endpoints:
+            user_endpoints = endpoints.keys()
 
-    limit = int(user_limit) if user_limit else None
+        limit = int(user_limit) if user_limit else None
 
-    print("\nStarting Extraction Process.\n")
-    extract_data(
-        {key: endpoints[key] for key in user_endpoints}, user_output_format, limit
-    )
-    print("\nExtraction process complete.\n")
+        print("\nStarting Extraction Process.\n")
+        extract_data(
+            {key: endpoints[key] for key in user_endpoints}, user_output_format, limit
+        )
+        print("\nExtraction process complete.\n")
+    elif len(sys.argv) > 1 and sys.argv[1] == "clean":
+        print("\nStarting Cleaning Process.\n")
+        clean_data()
+        print("\nCleaning process complete.\n")
+    elif len(sys.argv) > 1 and sys.argv[1] == "compute":
+        print("\nStarting Computation Process.\n")
+        compute_data(user_output_format)
+        print("\nComputation process complete.\n")
+    elif len(sys.argv) > 1 and sys.argv[1] == "visualize":
+        print("\nStarting Visualization Process.\n")
+        visualize_data()
+        print("\nVisualization process complete.\n")
+    else:
+        user_endpoints = input(
+            "Enter entities (contacts,campaigns,tags... or 'all' for all entities): "
+        ).split(",")
+        user_output_format = input("Enter output format (csv or xlsx): ")
+        user_limit = input(
+            "Enter the maximum number of data points to fetch (or leave empty for all): "
+        )
 
-    print("\nStarting Cleaning Process.\n")
-    clean_data()
-    print("\nCleaning process complete.\n")
+        if "all" in user_endpoints:
+            user_endpoints = endpoints.keys()
 
-    print("\nStarting Computation Process.\n")
-    compute_data(user_output_format)
-    print("\nComputation process complete.\n")
+        limit = int(user_limit) if user_limit else None
 
-    print("\nStarting Visualization Process.\n")
-    # visualize_data()
-    print("\nVisualization process complete.\n")
+        print("\nStarting Extraction Process.\n")
+        extract_data(
+            {key: endpoints[key] for key in user_endpoints}, user_output_format, limit
+        )
+        print("\nExtraction process complete.\n")
 
-    print("\nPlease check the 'exports' directory for all the resultant data.\n")
+        print("\nStarting Cleaning Process.\n")
+        clean_data()
+        print("\nCleaning process complete.\n")
+
+        print("\nStarting Computation Process.\n")
+        compute_data(user_output_format)
+        print("\nComputation process complete.\n")
+
+        print("\nStarting Visualization Process.\n")
+        visualize_data()
+        print("\nVisualization process complete.\n")
+
+        print("\nPlease check the 'exports' directory for all the resultant data.\n")
